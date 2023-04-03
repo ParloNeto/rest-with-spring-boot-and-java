c0ceb495ec1d26efbea6eba9358b3021d45540f2
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-04-03 11:36:34 -0300

    Serializando YML

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/config/WebConfig.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/config/WebConfig.java
@@ -1,12 +1,26 @@
 package br.com.paulo.config;
 
+import br.com.paulo.serialization.converter.YamlJackson2HttpMesageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+import static br.com.paulo.util.MediaType.APPLICATION_YML;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf(APPLICATION_YML);
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add((new YamlJackson2HttpMesageConverter()));
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 //       configurer.favorParameter(true).
@@ -19,7 +33,11 @@ public class WebConfig implements WebMvcConfigurer {
         configurer.favorParameter(false).
                 ignoreAcceptHeader(false).
                 useRegisteredExtensionsOnly(false).
-                    defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON).
-                    defaultContentType(MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+                    defaultContentType(MediaType.APPLICATION_JSON)
+                        .mediaType("json", MediaType.APPLICATION_JSON)
+                        .mediaType("xml", MediaType.APPLICATION_XML)
+                        .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
+                        ;
+
     }
 }
--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/controllers/PersonController.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/controllers/PersonController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import br.com.paulo.data.vo.v1.PersonVO;
 import br.com.paulo.services.PersonServices;
+import br.com.paulo.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static br.com.paulo.util.MediaType.APPLICATION_YML;
 
 
 @RestController
@@ -19,32 +22,31 @@ public class PersonController {
     ///private PersonServices service = new PersonServices();
 
     @GetMapping(
-            produces = { MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE })
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
     public List<PersonVO> findAll() {
         return service.findAll();
     }
 
     @GetMapping(
             value = "/{id}",
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
     public PersonVO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = { MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE })
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
+    )
     public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
     }
 
     @PutMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = { MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE })
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
     public PersonVO update(@RequestBody PersonVO person) {
+
         return service.update(person);
     }
 
