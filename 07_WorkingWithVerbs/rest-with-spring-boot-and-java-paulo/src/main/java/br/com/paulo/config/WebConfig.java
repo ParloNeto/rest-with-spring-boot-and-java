73c1a85c52de012e8de194c290dd6a57cdc23832
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-05-03 14:12:33 -0300

    Habilitando o CORS de Forma Global

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/config/WebConfig.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.paulo.config;
 
 import br.com.paulo.serialization.converter.YamlJackson2HttpMesageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,10 +18,23 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf(APPLICATION_YML);
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+//                .allowedMethods("GET", "POST", "PUT")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add((new YamlJackson2HttpMesageConverter()));
     }
+
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -40,4 +55,6 @@ public class WebConfig implements WebMvcConfigurer {
                         ;
 
     }
+
+
 }
