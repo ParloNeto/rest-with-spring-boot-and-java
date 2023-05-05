c0d5d6317dd0aa190894f155f5f958248548eca8
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-05-05 11:44:15 -0300

    .

---
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/test/java/integrationtests/controller/withjson/PersonControllerJsonTest.java
@@ -0,0 +1,20 @@
+//package integrationtests.controller.withjson;
+//
+//
+//import static io.restassured.RestAssured.given;
+//
+//import configs.TestConfigs;
+//import integrationtests.testcontainers.AbstractIntegrationTest;
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//public class PersonControllerJsonTest extends AbstractIntegrationTest {
+//
+//	@Test
+//	public void shouldDisplaySwaggerUiPage() {
+//
+//	}
+//
+//}
