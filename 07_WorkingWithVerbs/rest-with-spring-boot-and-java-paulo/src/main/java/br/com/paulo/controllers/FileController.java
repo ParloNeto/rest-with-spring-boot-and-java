78302c03f7ffe69a8729ab2821e2e4d6bcc96efa
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-06-02 23:09:23 -0300

    Implementando o Upload de vários arquivos simultaneamente

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/controllers/FileController.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/controllers/FileController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Tag(name = "File Endpoint")
 @RestController
@@ -32,8 +35,17 @@ public class FileController {
                 .path("/api/file/v1/downloadFile/")
                 .path(filename)
                 .toUriString();
-
         return
             new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        logger.info("Storing files to disk");
+
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
     }
 }
