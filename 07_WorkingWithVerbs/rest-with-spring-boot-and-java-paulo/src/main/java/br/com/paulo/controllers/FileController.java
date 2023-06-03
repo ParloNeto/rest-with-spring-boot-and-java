b8a4eaeec302a340e342f1b93744a5dba4b0c1ee
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-06-02 23:42:14 -0300

    Implementando o Download de Arquivos

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/controllers/FileController.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/src/main/java/br/com/paulo/controllers/FileController.java
@@ -5,12 +5,13 @@ import br.com.paulo.exceptions.MyFileNotFoundException;
 import br.com.paulo.services.FileStorageService;
 import br.com.paulo.services.PersonServices;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -49,6 +50,27 @@ public class FileController {
                 .stream()
                 .map(file -> uploadFile(file))
                 .collect(Collectors.toList());
+    }
+
+    //MY_file.txt
+    @GetMapping("/downloadFile/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename, HttpServletRequest request) {
+
+        logger.info("Reading a file on disk");
+
+        Resource resource = service.loadFileAsResource(filename);
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+            if (contentType.isBlank()) contentType = "application/octet-stream";
+        } catch (Exception e)
+        {   logger.info("Could not determine file type!");
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 
