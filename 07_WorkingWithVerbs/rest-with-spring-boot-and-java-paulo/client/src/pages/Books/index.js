9a6bf2325330974a3de0fd23351a0cf58be21fcf
ParloNeto <105455827+ParloNeto@users.noreply.github.com>
2023-06-18 21:39:32 -0300

    Implementando Logout e Delete

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/client/src/pages/Books/index.js
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-paulo/client/src/pages/Books/index.js
@@ -12,17 +12,40 @@ import logo from '../../assets/logo.png';
 function Books() {
 
     const [books, setBooks] = useState([]);
+
     const username = localStorage.getItem('username');
     const accessToken = localStorage.getItem('accessToken');
 
-    const userUpper = username.toString;
-
     const navigate = useNavigate();
+
+    async function logout() {
+        localStorage.clear();
+        navigate('/');
+    }
+
+    async function deleteBook(id) {
+        try {
+            await api.delete(`api/book/v1/${id}`, {
+                headers: {
+                    Authorization:  `Bearer ${accessToken}`
+                }
+            });
+
+            setBooks(books.filter(book => book.id !== id));
+        } catch (err) {
+            alert("Delete failed! Try again.");
+        }
+    }
 
     useEffect(() => {
         api.get('api/book/v1' , {
             headers: {
                 Authorization:  `Bearer ${accessToken}`
+            },
+            params: {
+                page: 1,
+                limit: 4,
+                direction: 'asc'
             }
         }).then(response => {
             setBooks(response.data._embedded.bookVOList)
@@ -33,8 +56,8 @@ function Books() {
             <header>
                 <img id="logo" src={logo} alt="Paulo Logo" />
                 <span>Welcome, <strong>{username.toUpperCase()}</strong>!</span>
-                <Link className="button" to="book/new">Add New Book</Link>
-                <button type="button">
+                <Link className="button" to="/books/book/new">Add New Book</Link>
+                <button onClick={logout} type="button">
                     <FiPower size={18} color="#251FC5"/>
                 </button>
             </header>
@@ -42,7 +65,7 @@ function Books() {
             <h1>Registered Books</h1>
             <ul>
                 {books.map(book => (
-                    <li>
+                    <li key={book.id}>
                     <strong>Title:</strong>
                     <p>{book.title}</p>
                     <strong>Author:</strong>
@@ -56,7 +79,7 @@ function Books() {
                         <FiEdit size={20} color="#251FC5"/>
                     </button>
 
-                    <button type="button">
+                    <button onClick={() => deleteBook(book.id)} type="button">
                         <FiTrash2 size={20} color="#251FC5"/>
                     </button>
                 </li>
